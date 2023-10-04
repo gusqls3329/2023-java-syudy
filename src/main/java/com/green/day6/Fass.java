661ef16d6bf6179999e9785fadb8f2ee34b8ffbf
김현빈 <gusqls81017@gmail.com>
2023-10-04 15:15:50 +0900

    sdfdfdf

--- src/main/java/com/green/day6/Fass.java
+++ src/main/java/com/green/day6/Fass.java
@@ -1,6 +1,6 @@
 package com.green.day6;
 
-public class fass {
+public class Fass {
     /* 깃허브 단어
     remote : 원격저장소, local : 내pc
     add: local에서 올리기 위해 올릴 파일들을 리스트 업 하는작업
@@ -8,4 +8,7 @@ public class fass {
     push : local에 원격으로 보내는것
     pull : 다른사람이 올려둔거 다운로드 받는것
      */
+    public static void main(String[] args) {
+
+    }
 }
