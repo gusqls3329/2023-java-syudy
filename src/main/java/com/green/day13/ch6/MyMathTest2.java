919a2bc39a78436254a1df65cb4644b76fe2b949
김현빈 <gusqls81017@gmail.com>
2023-10-17 14:24:19 +0900

    23.10.13_2 study

--- src/main/java/com/green/day13/ch6/MyMathTest2.java
+++ src/main/java/com/green/day13/ch6/MyMathTest2.java
@@ -2,6 +2,7 @@ package com.green.day13.ch6;
 
 public class MyMathTest2 {
     public static void main(String[] args) {
+        MyMath2.staticNum =1;
         MyMath2 mm2 = new MyMath2();
         mm2.num = 10;
         mm2.staticNum = 30;
@@ -15,5 +16,9 @@ public class MyMathTest2 {
 
 
         System.out.printf("MyMath2.staticNum = %d\n", MyMath2.staticNum);
+        /*1. System.out : 멤버필드 : 소괄호가 있으면 메소드 없으면 멤버필드
+          2. System : 대문자로 시작하기 때문에  클래스 이름이고 클래스 이름을 바로 가져왔기 때문에  그 클래스에 스태틱이 붙음
+          3. System.out.printf : .을사용했기 때문에 객체 주소값을 가지는 것을 알수있음
+          4. class. = static만 사용가능 다른 . 은 객체주소값을 가지는 것을 뜻함.*/
     }
 }
---
+++ src/main/java/com/green/day14/ch6/CardDeck.java
@@ -0,0 +1,53 @@
+package com.green.day14.ch6;
+
+public class CardDeck {
+    Card[] cards;
+
+    public CardDeck() {
+        cards = new Card[52];
+        String[] patterns = {"Spade", "Heart", "Diamond", "Club"};
+        int idx = 0;
+        for (int i = 0; i < patterns.length; i++) {
+            String pattern = patterns[i];
+            for (int j = 1; j <= 13; j++) {
+                String drnomination = getNumberFromInt(j);
+                Card c = new Card(pattern, drnomination);
+                cards[idx++] = c;
+            }
+        }
+
+    }
+
+    public void printAll() {
+        for (Card c : cards) {
+            c.printMySelf();
+        }
+    }
+
+    public Card getCard() {
+        Card c = null;
+        while (true) {
+            int rIdx = (int) (Math.random() * (cards.length));
+            c = cards[rIdx];
+            if (c != null) {
+                cards[rIdx] = null;
+                break;
+            }
+        }return c;
+    }
+
+    private String getNumberFromInt(int num) { //private은 같은 class에서만 사용가능,외부에서 주소 값을 가지고 있어도 사용불가
+        switch (num) {
+            case 1:
+                return "A";
+            case 11:
+                return "J";
+            case 12:
+                return "Q";
+            case 13:
+                return "K";
+        }
+        return String.valueOf(num); //정수를 문자열로
+    }
+
+}
