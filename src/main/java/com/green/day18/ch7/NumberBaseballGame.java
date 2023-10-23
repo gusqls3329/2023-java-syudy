1965fc1c890d19635a2ff673cce4bd61e14661c6
김현빈 <gusqls81017@gmail.com>
2023-10-23 17:24:23 +0900

    23.10.23.04

--- src/main/java/com/green/day18/ch7/NumberBaseballGame.java
+++ src/main/java/com/green/day18/ch7/NumberBaseballGame.java
@@ -1,5 +1,7 @@
 package com.green.day18.ch7;
 
+import java.util.Scanner;
+
 public class NumberBaseballGame {
     public static void main(String[] args) {
         NumberBaseball nbb = new NumberBaseball(3);
@@ -8,11 +10,62 @@ public class NumberBaseballGame {
 
 }
 
-class NumberBaseball{
-    NumberBaseball(int n){
+class NumberBaseball {
+    private final int GAME_COUNT;
+    private final int[] gameNumbers;
+
+    NumberBaseball(int count) {
+        GAME_COUNT = count;
+        gameNumbers = new int[count];
+    }
+
+
+    public void start() {
+        setRandomNumNotDuplicate();
 
     }
-    public void start(){
 
+    public void setRandomNumNotDuplicate() {
+        int num = 10;
+        for (int i = 0; i< num ; i++) {
+            int randon = (int) (Math.random() * 10);
+            gameNumbers[i] = randon;
+            for (int j = 0; j < i; j++) {
+                if(gameNumbers[i] == gameNumbers[j]){
+                    i--;
+                    break;
+                }
+            }
+        }
+    }
+    public void getUserInput(){
+        Scanner scan = new Scanner(System.in);
+        int[] userArr = new int[GAME_COUNT];
+        for (int i = 0; i < GAME_COUNT; i++) {
+            System.out.printf("%d 숫자 >> ",i + 1);
+            userArr[i] = scan.nextInt();
+        }
+
+        boolean r = isContinueCheckResult(userArr);
+        //빨간 줄 안생기게
+
+        scan.close();
+    }
+    private boolean isContinueCheckResult(int[] arr) {
+        int strike=0, ball=0, out;
+        for(int i=0; i<gameNumbers.length; i++) {
+            for(int z=0; z<arr.length; z++) {
+                if(gameNumbers[i] == arr[z]) {
+                    if(i == z) {
+                        strike++;
+                    } else {
+                        ball++;
+                    }
+                }
+            }
+        }
+        out = GAME_COUNT - (strike + ball);
+
+        return false;
     }
 }
