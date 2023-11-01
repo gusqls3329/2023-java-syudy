a93449acb3a597bfb26d53f5fca3ce9bc3bf3cd4
김현빈 <gusqls81017@gmail.com>
2023-11-01 11:08:59 +0900

    23.10.25.02

--- src/main/java/com/green/day24/BoardDao.java
+++ src/main/java/com/green/day24/BoardDao.java
@@ -2,28 +2,48 @@ package com.green.day24;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
-public class BoardDao {
-    public static int insBoard(BoardEntity entity){
+public class BoardDao {//Dao : 데이터 엑세스 오브젝트
+
+    public static int insBoard(BoardEntity entity) { //static or singleton둘중 하나를 사용해서 만들어야함.
         int result = 0;
         String sql = "INSERT INTO board (title,ctnts,writer) " +
-                        "VALUES" +
-                        "(?,?,?)";
+                "VALUES" +
+                "(?,?,?)";
 
         System.out.println(sql);
         try {
-            Connection con = MyConn.getConn();
-            PreparedStatement ps = con.prepareStatement(sql);
+            Connection conn = MyConn.getConn();
+            PreparedStatement ps = conn.prepareStatement(sql);
             ps.setString(1, entity.getTitle());
             ps.setString(2, entity.getCtnts());
             ps.setString(3, entity.getWriter());
 
             result = ps.executeUpdate();
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-        }finally {
+        } finally {
 
         }
         return result;
+    }
+
+    public static void close(Connection conn, PreparedStatement ps) {
+        if (ps != null) {
+            try {
+                ps.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 }
